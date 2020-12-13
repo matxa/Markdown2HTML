6e573d7fd2e83c0c8b5c6ee62c909727d019a955
matxa <42968545+matxa@users.noreply.github.com>
2020-12-13 11:33:28 -0500

    convert ul and ol

--- markdown2html.py
+++ markdown2html.py
@@ -1,26 +1,68 @@
 #!/usr/bin/python3
+"""Markdown to HTML converter script"""
 from sys import argv
 import os.path
 from os import path
 
-
+# Check for Usage
 if len(argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html")
     exit(1)
 elif not path.exists(argv[1]):
     print("Missing {}".format(argv[1]))
     exit(1)
-else:
-    html_lines = []
-    with open(argv[1], 'r') as src:
-        lines = src.readlines()
-    with open(argv[2], 'w') as dest:
-        for line in lines:
-            count_hash = line.count('#')
+
+html = []
+unordered_list = ''
+ordered_list = ''
+with open(argv[1], 'r') as src:
+    lines = src.readlines()
+    lines[-1] = lines[-1].replace('\n', '')
+with open(argv[2], 'w') as dest:
+    for line in lines:
+        # convert markdown headings ( # ) to html heading levels h1 - h6
+        count_hash = line.count('#')
+        if count_hash >= 1:
             line_no_hash = line.replace('#' * count_hash + ' ', '')
             line_no_hash = line_no_hash.replace('\n', '')
             line_html = "<h{}>{}</h{}>\n".format(
                 count_hash, line_no_hash, count_hash)
-            html_lines.append(line_html)
-        dest.writelines(html_lines)
-    exit(0)
+            html.append(line_html)
+
+        # convert markdown unordered list to html unordered list
+        if '-' in line[0] and ' ' in line[1]:
+            if '<li>' not in unordered_list:
+                unordered_list = '<ul>\n'
+            line_cp = line.replace('- ', '')
+            line_cp = line_cp.replace('\n', '')
+            unordered_list = unordered_list + "\t<li>{}</li>\n".format(line_cp)
+            if lines.index(line) + 1 < len(lines):
+                if '- ' not in lines[lines.index(line) + 1]:
+                    unordered_list = unordered_list + '</ul>\n'
+                    html.append(unordered_list)
+                    unordered_list = ''
+            else:
+                unordered_list = unordered_list + '</ul>\n'
+                html.append(unordered_list)
+                unordered_list = ''
+
+        # convert markdown ordered list to html ordered list
+        if '*' in line[0] and ' ' in line[1]:
+            if '<li>' not in ordered_list:
+                ordered_list = '<ol>\n'
+            line_cp = line.replace('* ', '')
+            line_cp = line_cp.replace('\n', '')
+            ordered_list = ordered_list + "\t<li>{}</li>\n".format(line_cp)
+            if lines.index(line) + 1 < len(lines):
+                if '* ' not in lines[lines.index(line) + 1]:
+                    ordered_list = ordered_list + '</ol>\n'
+                    html.append(ordered_list)
+                    ordered_list = ''
+            else:
+                ordered_list = ordered_list + '</ol>\n'
+                html.append(ordered_list)
+                ordered_list = ''
+
+    # write HTML to destenation file
+    dest.writelines(html)
+exit(0)
