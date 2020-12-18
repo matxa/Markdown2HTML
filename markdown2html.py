c1e771efe34204e7b766cdd117fd25c9ead3103f
matxa <42968545+matxa@users.noreply.github.com>
2020-12-18 12:42:23 -0500

    eprint

--- markdown2html.py
+++ markdown2html.py
@@ -3,14 +3,20 @@
 from sys import argv
 import os.path
 from os import path
+import sys
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 
 if __name__ == "__main__":
     # Check for Usage
     if len(argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        eprint("Usage: ./markdown2html.py README.md README.html")
         exit(1)
     elif not path.exists(argv[1]):
-        print("Missing {}".format(argv[1]))
+        eprint("Missing {}".format(argv[1]))
         exit(1)
 
     html = []
