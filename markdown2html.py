d7812019655e8a7bdbcab25eee214583dbdc99fc
matxa <42968545+matxa@users.noreply.github.com>
2020-12-17 20:31:17 -0500

    102

--- markdown2html.py
+++ markdown2html.py
@@ -3,13 +3,15 @@
 from sys import argv
 import os.path
 from os import path
+import sys
+
 
 # Check for Usage
 if len(argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html")
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     exit(1)
 elif not path.exists(argv[1]):
-    print("Missing {}".format(argv[1]))
+    print("Missing {}".format(argv[1]), file=sys.stderr)
     exit(1)
 
 html = []
